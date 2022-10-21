dc96e798d6dfd9af27b16457beaf780a71368ddd
AACHOURMOHAMED <aachourmohameddeveloper@gmail.com>
2022-10-21 08:40:31 +0100

    Add Methods to user and post and likes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :posts
   belongs_to :users
+
+  # A method that updates the comments counter for a post.
+  def update_post_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
